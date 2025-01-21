edf4e4a3e0e5ae7a743b3fc0a1b63d9cf2993d67
rydlewiczc <rydlewiczc@duck.com>
2025-01-21 08:04:16 +0800

    Add initial index page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Well Control Bridging Document Form</h1>
+      {/* Form will be added here */}
+    </div>
+  );
+}
